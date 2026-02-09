a56bb28891dcae75c006dbcb269a0628d8c2542b
choeihwa <chldlghk@gmail.com>
2026-02-09 14:31:51 +0900

    fix: add iam role to ec2 and update instance type for private and internal

--- terraform/main/modules/ec2-instance/main.tf
+++ terraform/main/modules/ec2-instance/main.tf
@@ -1,3 +1,66 @@
+resource "aws_iam_policy" "ssm_parameter_policy" {
+  name        = "myce-ssm-parameter-policy"
+  description = "Allow access to SSM Parameter Store"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid": "AllowReadMyceParameterStore",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:GetParametersByPath",
+            ],
+            "Resource": [
+                "arn:aws:ssm:ap-northeast-2:*:parameter/myce/*",
+                "arn:aws:ssm:ap-northeast-2:*:parameter/myce"
+            ]
+        Resource = "*"
+      },
+      {
+            "Sid": "AllowDecryptForParameterStore",
+            "Effect": "Allow",
+            "Action": "kms:Decrypt",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "ssm.ap-northeast-2.amazonaws.com"
+                }
+            }
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "myce-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ssm_parameter_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "myce-ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 # EC2 생성
 resource "aws_instance" "public_instance" {
   ami                         = var.ami
@@ -6,26 +69,29 @@ resource "aws_instance" "public_instance" {
   subnet_id                   = var.subnet_ids.public
   vpc_security_group_ids      = var.security_goups.public
   associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   tags                        = { Name = "${var.name_prefix}-public" }
 }
 
 resource "aws_instance" "private_instance" {
   ami                         = var.ami
-  instance_type               = var.instance_type
+  instance_type               = "t3.micro"
   key_name                    = var.key_pair_name
   subnet_id                   = var.subnet_ids.private
   vpc_security_group_ids      = var.security_goups.private
   associate_public_ip_address = false
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   tags                        = { Name = "${var.name_prefix}-private" }
 }
 
 resource "aws_instance" "internal_instance" {
   ami                         = var.ami
-  instance_type               = var.instance_type
+  instance_type               = "t3.micro"
   key_name                    = var.key_pair_name
   subnet_id                   = var.subnet_ids.internal
   vpc_security_group_ids      = var.security_goups.internal
   associate_public_ip_address = false
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   tags                        = { Name = "${var.name_prefix}-internal" }
 }
 
@@ -37,6 +103,7 @@ resource "aws_instance" "monitoring_instance" {
   vpc_security_group_ids      = var.security_goups.monitoring
   associate_public_ip_address = true
   source_dest_check           = false
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   tags                        = { Name = "${var.name_prefix}-monitoring" }
 }
 
